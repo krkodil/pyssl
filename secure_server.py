e34035cc8d8be1582135a3ad90978fe949835a3f
krkodil <ktassimir.koychev@gmail.com>
2019-10-31 15:29:48 +0200

    server_thread

--- secure_server.py
+++ secure_server.py
@@ -1,36 +1,49 @@
 import socket
 import ssl
+from _thread import *
 
 HOST = 'localhost'
 PORT = 10443
+
+
+def server_thread(server):
+    while True:
+        client, address = server.accept()
+        print('Accepted connection from {}:{}'.format(address[0], address[1]))
+        secure_sock = ssl.wrap_socket(client,
+                                      server_side=True,
+                                      ca_certs="keys/clients.pem",
+                                      certfile="keys/server.crt",
+                                      keyfile="keys/server.key",
+                                      cert_reqs=ssl.CERT_REQUIRED,
+                                      ssl_version=ssl.PROTOCOL_TLSv1_2)
+
+        print(secure_sock.cipher())
+
+        cert = secure_sock.getpeercert()
+        print(cert)
+        start_new_thread(client_thread, (secure_sock, address[0]))
+
+
+def client_thread(client, client_ip):
+    while True:
+        try:
+            request = client.recv(255)
+        except ConnectionResetError:
+            break
+        if request:
+            client.send(request)
+        break
+    print('Client {} disconnected.'.format(client_ip))
+    client.close()
+
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind((HOST, PORT))
 server_socket.listen(10)
 
-client, address = server_socket.accept()
-secure_sock = ssl.wrap_socket(client,
-                              server_side=True,
-                              ca_certs="keys/clients.pem",
-                              certfile="keys/server.crt",
-                              keyfile="keys/server.key",
-                              cert_reqs=ssl.CERT_REQUIRED,
-                              ssl_version=ssl.PROTOCOL_TLSv1_2)
+start_new_thread(server_thread, (server_socket, ))
 
-print(secure_sock.cipher())
-
-cert = secure_sock.getpeercert()
-print(cert)
-
-
-# verify client
-if not cert or ('organizationName', 'Test') not in cert['subject'][3]:
-    raise Exception("Client cert not match!")
-
-try:
-    data = secure_sock.read(1024)
-    secure_sock.write(data)
-finally:
-    secure_sock.close()
-    server_socket.close()
+input('Press \'Enter\' to stop the server ...\n\n')
+server_socket.close()
